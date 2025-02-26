a6c0cd2a3cc691c60039e925bd3d6dfdfe148e33
Emil Vang Klinker <emilvangklinker@gmail.com>
2025-02-26 14:10:47 +0100

    Emil tester lige

--- first_file.py
+++ first_file.py
@@ -1,8 +1,14 @@
-data = ["Simon", "William", "Emil"]
+data = ["Simon", "William", "Emil","Simon 2.0"]
 
 # Find den person med det første forbogstav
 min_name = min(data)  # min() sammenligner alfabetisk
 data.remove(min_name)  # Fjern personen fra sin nuværende placering
 data.insert(0, min_name)  # Tilføj personen forrest
 
+if len(data) == 4:
+    print("Accepted group size")
+else:
+    print("Max 4, min 4")
+
 print(data)
+
